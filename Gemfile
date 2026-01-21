ecf56a7648fe50414b4ecfb738157ecef0703fdb
Bart <bart.vanderwal@han.nl>
2026-01-21 16:28:49 +0100

    Upgrade dependencies: Node, Ruby, en npm packages
    Verhoog minimale versies voor veiligheid en stabiliteit:
    - Node van 18 naar 20 LTS in serve.sh
    - Ruby bundler dependencies in Gemfile/Gemfile.lock
    - npm packages in package.json
    Geen breaking changes in functionaliteit.

--- Gemfile
+++ Gemfile
@@ -2,10 +2,12 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'webrick'
+gem 'html-proofer'
 
 group :jekyll_plugins do
   gem "jekyll-paginate"
   gem "jekyll-feed"
   gem "jekyll-sitemap"
   gem "jekyll-redirect-from"
+  gem "jekyll-reading-time"
 end
--- serve.sh
+++ serve.sh
@@ -4,4 +4,17 @@ export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 export GEM_HOME="$HOME/.gem/ruby/3.4.0"
 export PATH="$GEM_HOME/bin:$PATH"
 
+# Check if 'check' argument is passed to validate links
+if [ "$1" == "check" ]; then
+  # Build site first
+  echo "Building site..."
+  bundle exec jekyll build
+  
+  # Then validate internal links (disable external link checking)
+  echo "Checking internal links..."
+  bundle exec htmlproofer ./_site --disable-external
+  exit $?
+fi
+
+# Default: start dev server
 bundle exec jekyll serve --host 0.0.0.0
